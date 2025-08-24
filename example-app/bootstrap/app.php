26a6c875c8e472d4a9c7e858e1566efd1e08fce8
awada9201 <awada9201@gmail.com>
2025-08-24 11:14:48 +0300

    login & signup and logout features

--- example-app/bootstrap/app.php
+++ example-app/bootstrap/app.php
@@ -3,15 +3,22 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Fruitcake\Cors\HandleCors; // Add this import
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php', // Make sure this is included
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Add CORS middleware globally
+        $middleware->append(HandleCors::class);
+        
+        // Or add it to specific middleware groups
+        // $middleware->api(prepend: [HandleCors::class]);
+        // $middleware->web(prepend: [HandleCors::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- example-app/react/src/axios-client.js
+++ example-app/react/src/axios-client.js
@@ -5,9 +5,13 @@ const axiosClient = axios.create({
 })
 
 axiosClient.interceptors.request.use((config) =>{
-    const token = localStorage.get('ACCESS_TOKEN');
-    config.headers.Authorization = `Bearer ${token}`
-    return config;
+    try{
+        const token = localStorage.getItem('ACCESS_TOKEN');
+        config.headers.Authorization = `Bearer ${token}`
+        return config;
+    } catch (err){
+        console.log(err);
+    }
 })
 
 axiosClient.interceptors.response.use((response) =>{
